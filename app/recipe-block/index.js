5ac8e4d276d7bd2a666b0e8ecb5fd9b9d863fdf8
Middi <richard@richardmiddleton.me>
2019-12-27 16:04:12 +0000

    add controls

--- app/recipe-block/index.js
+++ app/recipe-block/index.js
@@ -2,6 +2,8 @@ import block_icons from '../icons';
 
 const {registerBlockType} = wp.blocks;
 const {__} = wp.i18n;
+const {InspectorControls} = wp.blockEditor;
+const {PanelBody, PanelRow, TextControl, SelectControl} = wp.components;
 
 registerBlockType(
     'udemy/recipe',
@@ -20,7 +22,62 @@ registerBlockType(
         },
         edit: (props) => {
             // console.log(props);
-            return (
+            return [
+                <InspectorControls>
+                    <PanelBody title={__('Basics', 'recipe')}>
+                        <PanelRow>
+                            <p>{__('Configure the contents of the block here.', 'recipe')}</p>
+                        </PanelRow>
+                        <TextControl label={__('Ingredients', 'recipe')}
+                        help={__('example: tomatoes, lettuce, olive oil, etc.', 'recipe')}
+                        value="test"
+                        onChange={(new_val) => {
+                            console.log(new_val);
+                        }}
+                        />
+                        <TextControl label={__('Cooking Time', 'recipe')}
+                        help={__('How long will this take to cook?', 'recipe')}
+                        value="test"
+                        onChange={(new_val) => {
+                            console.log(new_val);
+                        }}
+                        />
+                        <TextControl
+                            label={__('Utensils', 'recipe')}
+                            help={__('What Utensils will you need?', 'recipe')}
+                            value="test"
+                            onChange={(new_val) => {
+                                console.log(new_val);
+                            }}
+                        />
+                        <SelectControl
+                            label={__( 'Cooking Experience', 'recipe')}
+                            help={__('How skilled should the user be?', 'recipe')}
+                            value="Beginner"
+                            options={[
+                                {value: 'Beginner', label: 'Beginner'},
+                                {value: 'Intermediate', label: 'Intermediate'},
+                                {value: 'Expert', label: 'Expert'}
+                            ]}
+                            onChange={(new_val) => {
+                                console.log(new_val)
+                            }}
+                        />
+                        <SelectControl
+                            label={__( 'Meal Type', 'recipe')}
+                            help={__('When is this best eaten?', 'recipe')}
+                            value="Breakfast"
+                            options={[
+                                {value: 'Breakfast', label: 'Breakfast'},
+                                {value: 'Lunch', label: 'Lunch'},
+                                {value: 'Dinner', label: 'Dinner'}
+                            ]}
+                            onChange={(new_val) => {
+                                console.log(new_val)
+                            }}
+                        />
+                    </PanelBody>
+                </InspectorControls>,
                 <div className={props.className}>
                     <ul class="list-unstyled">
                         <li><strong>{__('Ingredients', 'recipe')}: </strong> INGREDIENTS_PH</li>
@@ -30,7 +87,7 @@ registerBlockType(
                         <li><strong>{__('Meal Type', 'recipe')}: </strong> TYPE_PH</li>
                     </ul>
                 </div>
-            );
+            ];
         },
         save: (props) => {
             return <p>Hello World!</p>
