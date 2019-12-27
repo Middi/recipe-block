2de9ce0a539262541ee195ba63c89a3b087709f1
Middi <richard@richardmiddleton.me>
2019-12-27 15:28:18 +0000

    firs block commit

---
+++ app/recipe-block/index.js
@@ -0,0 +1,39 @@
+import block_icons from '../icons';
+
+const {registerBlockType} = wp.blocks;
+const {__} = wp.i18n;
+
+registerBlockType(
+    'udemy/recipe',
+    {
+        title: __('Recipe', 'recipe'),
+        description: __('Provides a short summary of a recipe.', 'recipe'),
+        category: 'common',
+        icon: block_icons.wapuu,
+        keywords: [
+            __('Food', 'recipe'),
+            __('Ingredients', 'recipe'),
+            __('Meal Type', 'recipe')
+        ],
+        supports: {
+            html: false
+        },
+        edit: (props) => {
+            // console.log(props);
+            return (
+                <div className={props.className}>
+                    <ul class="list-unstyled">
+                        <li><strong>{__('Ingredients', 'recipe')}: </strong> INGREDIENTS_PH</li>
+                        <li><strong>{__('Cooking Time', 'recipe')}: </strong> COOKING_TIME_PH</li>
+                        <li><strong>{__('Utensils', 'recipe')}: </strong> UTENSILS_PH</li>
+                        <li><strong>{__('Cooking Experience', 'recipe')}: </strong> LEVEL_PH</li>
+                        <li><strong>{__('Meal Type', 'recipe')}: </strong> TYPE_PH</li>
+                    </ul>
+                </div>
+            );
+        },
+        save: (props) => {
+            return <p>Hello World!</p>
+        }
+    }
+);
